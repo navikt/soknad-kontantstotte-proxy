32b94389276e5d4f120f7040bfb0d5982ab1e15d
Martine Enger <marenger@student.matnat.uio.no>
2018-08-06 12:51:27 +0200

    SD-416: Legger på logging på TPS-kall for testformål

--- src/main/java/no/nav/kontantstotte/proxy/service/ws/person/PersonServiceTpsWs.java
+++ src/main/java/no/nav/kontantstotte/proxy/service/ws/person/PersonServiceTpsWs.java
@@ -8,11 +8,14 @@ import no.nav.tjeneste.virksomhet.person.v3.binding.HentPersonSikkerhetsbegrensn
 import no.nav.tjeneste.virksomhet.person.v3.binding.PersonV3;
 import no.nav.tjeneste.virksomhet.person.v3.meldinger.HentPersonRequest;
 import no.nav.tjeneste.virksomhet.person.v3.meldinger.HentPersonResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PersonServiceTpsWs implements PersonService {
 
     private final PersonV3 personV3;
     private final PersonV3 healthIndicator;
+    private static final Logger LOG = LoggerFactory.getLogger(PersonServiceTpsWs.class);
 
     public PersonServiceTpsWs(PersonV3 personV3, PersonV3 healthIndicator) {
         this.personV3 = personV3;
@@ -29,8 +32,10 @@ public class PersonServiceTpsWs implements PersonService {
         HentPersonRequest request = RequestUtils.request(fnr);
         try {
             HentPersonResponse hentPersonResponse = this.personV3.hentPerson(request);
+            LOG.info("Personkall til TPS ok");
             return PersonMapper.person(hentPersonResponse.getPerson());
         } catch (HentPersonSikkerhetsbegrensning | HentPersonPersonIkkeFunnet e) {
+            LOG.error("Personkall til TPS gir feil:", e);
             throw new ServiceException(e);
         }
     }
