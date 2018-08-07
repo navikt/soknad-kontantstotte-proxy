6ffa1f6e380086b72715231555b859a0d390edc9
Martine Enger <marenger@student.matnat.uio.no>
2018-08-07 13:36:49 +0200

    [kaizen] Ryddet, fjernet logging for testformål etc

--- src/main/java/no/nav/kontantstotte/proxy/service/ws/person/PersonMapper.java
+++ src/main/java/no/nav/kontantstotte/proxy/service/ws/person/PersonMapper.java
@@ -1,11 +1,15 @@
 package no.nav.kontantstotte.proxy.service.ws.person;
 
 import no.nav.kontantstotte.proxy.domain.Person;
+import no.nav.tjeneste.virksomhet.person.v3.informasjon.Diskresjonskoder;
+
+import java.util.Optional;
 
 public class PersonMapper {
 
     public static Person person(no.nav.tjeneste.virksomhet.person.v3.informasjon.Person personV3) {
-        String diskresjonskode = personV3.getDiskresjonskode() != null ? personV3.getDiskresjonskode().getKodeverksRef() : null;
+        String diskresjonskode =  Optional.ofNullable(personV3.getDiskresjonskode())
+                .map(Diskresjonskoder::getKodeverksRef).orElse(null);
 
         return new Person.Builder()
                 .fornavn(personV3.getPersonnavn().getFornavn())
--- src/main/java/no/nav/kontantstotte/proxy/service/ws/person/PersonServiceTpsWs.java
+++ src/main/java/no/nav/kontantstotte/proxy/service/ws/person/PersonServiceTpsWs.java
@@ -9,14 +9,11 @@ import no.nav.tjeneste.virksomhet.person.v3.binding.HentPersonSikkerhetsbegrensn
 import no.nav.tjeneste.virksomhet.person.v3.binding.PersonV3;
 import no.nav.tjeneste.virksomhet.person.v3.meldinger.HentPersonRequest;
 import no.nav.tjeneste.virksomhet.person.v3.meldinger.HentPersonResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class PersonServiceTpsWs implements PersonService {
 
     private final PersonV3 personV3;
     private final PersonV3 healthIndicator;
-    private static final Logger LOG = LoggerFactory.getLogger(PersonServiceTpsWs.class);
 
     public PersonServiceTpsWs(PersonV3 personV3, PersonV3 healthIndicator) {
         this.personV3 = personV3;
@@ -33,14 +30,12 @@ public class PersonServiceTpsWs implements PersonService {
         HentPersonRequest request = RequestUtils.request(fnr);
         try {
             HentPersonResponse hentPersonResponse = this.personV3.hentPerson(request);
-            LOG.info("Personkall til TPS ok");
             Person person = PersonMapper.person(hentPersonResponse.getPerson());
             if(person.getDiskresjonskode() != null) {
                 throw new SikkerhetsbegrensningExeption("Ikke tilgang til å hente personinformasjon");
             }
             return person;
         } catch (HentPersonSikkerhetsbegrensning | HentPersonPersonIkkeFunnet e) {
-            LOG.error("Personkall til TPS gir feil:", e);
             throw new ServiceException(e);
         }
     }
