06a6e61477eeda70e22c422c0ca8dba81a8664a0
Ørjan Helstrøm <orjan.breimo.helstrom@nav.no>
2018-08-28 14:02:51 +0200

    fix-before-production * Legger på feature-toggle for dokmot integrasjon

--- src/main/java/no/nav/kontantstotte/proxy/api/rest/mottak/SoknadMottakResource.java
+++ src/main/java/no/nav/kontantstotte/proxy/api/rest/mottak/SoknadMottakResource.java
@@ -1,13 +1,11 @@
 package no.nav.kontantstotte.proxy.api.rest.mottak;
 
+import no.finn.unleash.Unleash;
 import no.nav.kontantstotte.proxy.innsending.dokument.domain.SoknadSender;
 import no.nav.security.oidc.api.ProtectedWithClaims;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
@@ -15,14 +13,19 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 @Path("soknad")
 public class SoknadMottakResource {
 
+    private static final String BRUK_DOKMOT_INTEGRASJON = "kontantstotte.integrasjon.dokmot";
+
     private final SoknadSender soknadSender;
 
     private final SoknadConverter converter = new SoknadConverter();
 
+    private final Unleash unleash;
+
     @Inject
-    public SoknadMottakResource(SoknadSender soknadSender) {
+    public SoknadMottakResource(SoknadSender soknadSender, Unleash unleash) {
 
         this.soknadSender = soknadSender;
+        this.unleash = unleash;
     }
 
     @POST
@@ -30,8 +33,11 @@ public class SoknadMottakResource {
     @Produces(APPLICATION_JSON)
     @ProtectedWithClaims(issuer = "selvbetjening", claimMap = { "acr=Level4" })
     public Response mottaSoknad(SoknadDto soknadDto) {
-        soknadSender.send(converter.toSoknad(soknadDto));
-        return Response.ok().build();
+        if(unleash.isEnabled(BRUK_DOKMOT_INTEGRASJON)) {
+            soknadSender.send(converter.toSoknad(soknadDto));
+            return Response.ok().build();
+        } else {
+            throw new WebApplicationException(Response.Status.NOT_IMPLEMENTED);
+        }
     }
-
 }
