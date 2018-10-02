9fda410a1e9c5160ea592efa64e652b7df066668
Einar Bjerve <einar.bjerve@nav.no>
2018-10-02 12:36:59 +0200

    Revert "Fjerna dato fra innkommende kall også"
    This reverts commit c2d7e696c9362cf9db70e55de1e762ef6ee94424.

--- src/test/java/no/nav/kontantstotte/proxy/api/rest/mottak/SoknadConverterTest.java
+++ src/test/java/no/nav/kontantstotte/proxy/api/rest/mottak/SoknadConverterTest.java
@@ -1,7 +1,6 @@
 package no.nav.kontantstotte.proxy.api.rest.mottak;
 
 import no.nav.kontantstotte.proxy.innsending.dokument.domain.Soknad;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
@@ -13,22 +12,21 @@ public class SoknadConverterTest {
     private SoknadConverter converter = new SoknadConverter();
 
     @Test
-    @Ignore
     public void at_soknad_uten_innsendt_dato_far_generert_dato() {
 
-        Soknad soknadUtenTimestamp = converter.toSoknad(new SoknadDto("MASKERT_FNR", "pdf".getBytes()));
+        Soknad soknadUtenTimestamp = converter.toSoknad(new SoknadDto("MASKERT_FNR", "pdf".getBytes(), null));
 
         assertThat(soknadUtenTimestamp.getInnsendingTimestamp()).isNotNull();
 
     }
 
     @Test
-    @Ignore
     public void at_soknad_med_innsendt_dato_beholdes() {
 
         LocalDateTime someDate = LocalDateTime.now().minusDays(10);
 
-        Soknad soknadMedTimestamp = converter.toSoknad(new SoknadDto("MASKERT_FNR", "pdf".getBytes()));
+        Soknad soknadMedTimestamp = converter.toSoknad(new SoknadDto("MASKERT_FNR", "pdf".getBytes(),
+                someDate));
 
         assertThat(soknadMedTimestamp.getInnsendingTimestamp()).isEqualTo(someDate);
     }
