791493a58a8315d9e07589015b477a5cc3ff2ffb
Martine Enger <marenger@student.matnat.uio.no>
2018-11-06 12:41:23 +0100

    Revert "SD-521: Trigger dokmot-feil ved innsending for å teste logging på nytt"
    This reverts commit 3001464111a344e406f155583246804e39957348.

--- src/test/java/no/nav/kontantstotte/proxy/innsending/dokument/dokmot/DokmotJmsSoknadSenderTest.java
+++ src/test/java/no/nav/kontantstotte/proxy/innsending/dokument/dokmot/DokmotJmsSoknadSenderTest.java
@@ -1,7 +1,6 @@
 package no.nav.kontantstotte.proxy.innsending.dokument.dokmot;
 
 import no.nav.kontantstotte.proxy.innsending.dokument.domain.Soknad;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.jms.core.JmsTemplate;
 
@@ -17,7 +16,6 @@ public class DokmotJmsSoknadSenderTest {
     private DokmotJMSSender soknadSender = new DokmotJMSSender(template, queueConfig);
 
     @Test
-    @Ignore
     public void that_disabled_queues_do_not_get_called() {
 
         when(queueConfig.isEnabled()).thenReturn(false);
@@ -28,7 +26,6 @@ public class DokmotJmsSoknadSenderTest {
     }
 
     @Test
-    @Ignore
     public void successful_send() {
         when(queueConfig.isEnabled()).thenReturn(true);
 
