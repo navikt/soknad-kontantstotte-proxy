37283c6dc3efa970f9699412def14226c7bbc1d3
Nina Kjekstad <nina.kjekstad@bekk-mac-01818.local>
2019-01-10 13:33:48 +0100

    [kaizen] Test for å undersøke brukeropplevelse ved innsendingsfeil

--- src/main/java/no/nav/kontantstotte/proxy/innsending/dokument/dokmot/DokmotJMSSender.java
+++ src/main/java/no/nav/kontantstotte/proxy/innsending/dokument/dokmot/DokmotJMSSender.java
@@ -38,6 +38,8 @@ public class DokmotJMSSender implements SoknadSender {
         }
 
         try {
+            throw new IllegalStateException("Test-exception");
+            /*
             template.send(session -> {
                 LOG.info("Sender SoknadsXML til DOKMOT");
                 TextMessage msg = session.createTextMessage(generator.toXML(soknad));
@@ -46,6 +48,7 @@ public class DokmotJMSSender implements SoknadSender {
                 return msg;
             });
             dokmotSuccess.increment();
+            */
         } catch (JmsException e) {
             dokmotFailure.increment();
             throw new DokmotQueueUnavailableException(e, queueConfig);
