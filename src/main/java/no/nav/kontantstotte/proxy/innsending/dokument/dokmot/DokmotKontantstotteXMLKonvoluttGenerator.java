78a588f2df903f7c56a7ec73690bb46df6ff8961
Martine Enger <martine.enger@nav.no>
2019-10-16 11:48:53 +0200

    Bruk Call-Id mot DOKMOT (#70)
    * Bruk call-Id i stedet for corr-id som kanalrefid mot dokmot. Bruk logfilter fra familie-felles for å sikre lik callid på tvers av våre apper.

--- src/main/java/no/nav/kontantstotte/proxy/innsending/dokument/dokmot/DokmotKontantstotteXMLKonvoluttGenerator.java
+++ src/main/java/no/nav/kontantstotte/proxy/innsending/dokument/dokmot/DokmotKontantstotteXMLKonvoluttGenerator.java
@@ -1,9 +1,9 @@
 package no.nav.kontantstotte.proxy.innsending.dokument.dokmot;
 
+import no.nav.familie.log.mdc.MDCConstants;
 import no.nav.kontantstotte.proxy.innsending.dokument.dokmot.conversion.Jaxb;
 import no.nav.kontantstotte.proxy.innsending.dokument.domain.Soknad;
 import no.nav.kontantstotte.proxy.innsending.dokument.domain.SoknadVedlegg;
-import no.nav.log.MDCConstants;
 import no.nav.melding.virksomhet.dokumentforsendelse.v1.*;
 import org.slf4j.MDC;
 
@@ -29,7 +29,7 @@ class DokmotKontantstotteXMLKonvoluttGenerator {
 
     String toXML(Soknad soknad) {
 
-        String ref = MDC.get(MDCConstants.MDC_CORRELATION_ID);
+        String ref = MDC.get(MDCConstants.MDC_CALL_ID);
 
         LocalDateTime innsendingsTidspunkt = LocalDateTime.ofInstant(soknad.getInnsendingsTidspunkt(), ZoneId.of("Europe/Paris"));
 
